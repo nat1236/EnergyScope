ff43177ef02d4e811a49f1a1aca6124963f32975
Paolo Thiran <paolo.thiran@gmail.com>
2021-09-24 16:24:13 +0200

    Minor error

--- energyscope/preprocessing/run_ES.py
+++ energyscope/preprocessing/run_ES.py
@@ -29,7 +29,7 @@ def run_ES(config):
             reserves = pd.read_csv('Reserves.csv')
             reserves.index = range(1, len(reserves) + 1)
             reserves = reserves.loc[:, 'ES']
-            reserves = pd.DataFrame(reserves / 1000 + 3)
+            reserves = pd.DataFrame(reserves / 1000)
             reserves.rename(columns={'ES': 'end_uses_reserve'}, inplace=True)
             print_td_data(Time_Series, config['ES_path'], config['step1_output'], end_uses_reserve=reserves)
         elif config['import_reserves']=='from_df':
