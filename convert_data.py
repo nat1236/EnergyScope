698b0896bf28ec1474b6db586a2dc57a9e67a3e9
duboisa1 <antoine-dubois@hotmail.com>
2021-11-29 16:31:16 +0100

    Updated Data.xlsx with data from ESTD_data.dat

--- convert_data.py
+++ convert_data.py
@@ -6,6 +6,7 @@ def generate_demand_csv():
     # Demand
     demand = pd.read_excel("Data_management/DATA.xlsx", sheet_name='2.3 EUD', index_col=0, header=1, usecols=range(5))
     demand.columns = [x.strip() for x in demand.columns]
+    demand.index = [x.strip() for x in demand.index]
 
     # Add additional information
     demand_aux = pd.read_csv("Data/User_data/aux_demand.csv", index_col=0)
@@ -35,12 +36,12 @@ def generate_resources_csv():
     # Rename and reorder columns
     resources.index.name = 'parameter name'
     resources = resources.reset_index()
-    resources = resources[['Category', 'Subcategory', 'parameter name', 'avail', 'gwp_op', 'c_op', 'CO2_op']]
-    resources.columns = ['Category', 'Subcategory', 'parameter name', 'Availability', 'Direct and indirect emissions',
-                         'Price', 'Direct emissions']
+    resources = resources[['Category', 'Subcategory', 'parameter name', 'avail', 'gwp_op', 'c_op', 'einv_op']]
+    # resources.columns = ['Category', 'Subcategory', 'parameter name', 'Availability', 'Direct and indirect emissions',
+    #                      'Price', 'Direct emissions']
 
     # Add a line with units
-    units = pd.Series(['', '', 'units', '[GWh/y]', '[ktCO2-eq./GWh]', '[Meuro/GWh]', '[ktCO2-eq./GWh]'],
+    units = pd.Series(['', '', 'units', '[GWh/y]', '[ktCO2-eq./GWh]', '[Meuro/GWh]', '[GWh/y]'],
                       index=resources.columns)
     resources = pd.concat((units.to_frame().T, resources), axis=0)
 
@@ -62,14 +63,15 @@ def generate_technologies_csv():
     technologies.index.name = 'parameter name'
     technologies = technologies.reset_index()
     technologies = technologies[['Category', 'Subcategory', 'Technologies name', 'parameter name', 'c_inv',  'c_maint',
-                                 'gwp_constr', 'lifetime', 'c_p', 'fmin_perc', 'fmax_perc', 'f_min', 'f_max']]
+                                 'gwp_constr', 'einv_constr', 'lifetime', 'c_p', 'fmin_perc', 'fmax_perc',
+                                 'f_min', 'f_max']]
 
     # Add a line with units
     units = pd.Series(['', '', 'Name (simplified)', 'Name (in model and documents)',
                        '[Meuro/GW],[Meuro/GWh],[Meuro/(Mkmpass/h)],[Meuro/(Mtonkm/h)]',
                        '[Meuro/GW],[Meuro/GWh],[Meuro/(Mkmpass/h)],[Meuro/(Mtonkm/h)]',
                        '[ktonCO2_eq/GW],[ktonCO2_eq/GWh],[ktonCO2_eq/(Mkmpass/h)],[ktonCO2_eq/(Mtonkm/h)]',
-                       '[years]', '[]', '[]', '[]', '[GW]', '[GW]'],
+                       '[GWh/y]', '[years]', '[]', '[]', '[]', '[GW]', '[GW]'],
                       index=technologies.columns)
     technologies = pd.concat((units.to_frame().T, technologies), axis=0)
 
@@ -80,7 +82,7 @@ def generate_layers_csv():
 
     # Layers in-out
     layers = pd.read_excel("Data_management/DATA.xlsx", sheet_name='3.1 layers_in_out', index_col=1)
-    layers = layers.drop(layers.columns[[0, -1]], axis=1)
+    layers = layers.drop(layers.columns[0], axis=1)
     layers.columns = [x.strip() for x in layers.columns]
     layers.to_csv("Data/Developer_data/Layers_in_out.csv", sep=',')
 
@@ -88,17 +90,17 @@ def generate_layers_csv():
 def generate_storage_csv():
 
     # Storage eff in
-    storage_eff_in = pd.read_excel("Data_management/DATA.xlsx", sheet_name='3.3 STO', header=2, nrows=21, index_col=0)
+    storage_eff_in = pd.read_excel("Data_management/DATA.xlsx", sheet_name='3.3 STO', header=2, nrows=25, index_col=0)
     storage_eff_in.index = [x.strip() for x in storage_eff_in.index]
     storage_eff_in.to_csv("Data/Developer_data/Storage_eff_in.csv", sep=',')
 
     # Storage eff out
-    storage_eff_out = pd.read_excel("Data_management/DATA.xlsx", sheet_name='3.3 STO', header=26, nrows=21, index_col=0)
+    storage_eff_out = pd.read_excel("Data_management/DATA.xlsx", sheet_name='3.3 STO', header=30, nrows=25, index_col=0)
     storage_eff_out.index = [x.strip() for x in storage_eff_out.index]
     storage_eff_out.to_csv("Data/Developer_data/Storage_eff_out.csv", sep=',')
 
     # Storage characteristics
-    storage_c = pd.read_excel("Data_management/DATA.xlsx", sheet_name='3.3 STO', header=50, nrows=21, index_col=0)
+    storage_c = pd.read_excel("Data_management/DATA.xlsx", sheet_name='3.3 STO', header=58, nrows=25, index_col=0)
     storage_c.index = [x.strip() for x in storage_c.index]
     storage_c.dropna(axis=1).to_csv("Data/Developer_data/Storage_characteristics.csv", sep=',')
 
@@ -127,4 +129,4 @@ def estd_excel_to_csv():
 
 
 if __name__ == '__main__':
-    generate_time_series_csv()
+    generate_technologies_csv()
